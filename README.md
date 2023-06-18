e9fb7c1d480958ea823c5431f1543c3801e3e31d
stulnikau <pavel.stulnikov@connect.qut.edu.au>
2023-06-18 20:19:52 +1000

    Use template

---
+++ README.md
@@ -0,0 +1,15 @@
+# Go web demo: Mini Wiki
+
+## Running locally
+
+In your console, run
+
+```shell
+go run wiki.go
+```
+
+Then, navigate in your browser to:
+
+- `localhost:8080/view/{pagename}` to view a page
+- `localhost:8080/edit/{pagename}` to edit/create a page
+- In the edit page, click on "Save" to save the page
--- wiki.go
+++ wiki.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -24,9 +26,26 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	t, _ := template.ParseFiles("view.html")
+	t.Execute(w, p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w, p)
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample page")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	// http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
